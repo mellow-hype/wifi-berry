90ac4c1af6f6554f682443b31addb619ff7be027
mellow-hype <enrique.castillox@gmail.com>
2017-01-21 02:39:57 -0800

    fixed imports + passed presets to dnsmasq config function

--- core/modes.py
+++ core/modes.py
@@ -6,15 +6,21 @@ def automagic_install(default_settings_d):
 
 def wizard_install():
     # Import the configuration menus for IP, hostapd, and dnsmasq
-    from menu.wizard_main_menu import menu_wizard_ip
-    from menu.wizard_main_menu import menu_wizard_hostapd
-    from menu._dnsmasq_menu import menu_wizard_dnsmasq
+    from ..menu.wizard_main_menu import menu_wizard_ip
+    from ..menu.wizard_main_menu import menu_wizard_hostapd
+    from ..menu._dnsmasq_menu import menu_wizard_dnsmasq
 
-    # IP configuration
+    # -- Get IP configuration settings
     ip_config_d = menu_wizard_ip()
     
-    # hostapd configuration 
+    # -- Get hostapd configuration settings
     ap_config_d = menu_wizard_hostapd()
 
-    # dnsmasq configuration
-    dns_config_d = menu_wizard_dnsmasq(ip_config_d)
+    # -- Get dnsmasq configuration settings
+    # Pass previously set values to dnsmasq menu function 
+    dns_custom_d = {
+        "network": ip_config_d["network"],
+        "iface": ap_config_d["interface"]
+        }
+
+    dns_config_d = menu_wizard_dnsmasq(dns_custom_d)
