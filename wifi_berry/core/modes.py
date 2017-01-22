85d40e162b9ba14c5f021a5f336f8bec68668aaa
mellow-hype <enrique.castillox@gmail.com>
2017-01-22 15:43:54 -0800

    added automagic mode install logic

--- wifi_berry/core/modes.py
+++ wifi_berry/core/modes.py
@@ -1,7 +1,41 @@
+from .config import dnsmasq_conf_default_d
+from .config import hostapd_conf_default_d
+from .config import ip_conf_default_d
+from .config import BerryInit
+from .config import BerryConfig
 
-def automagic_install(default_settings_d):
-    # TODO: create automagic installation logic
-    pass
+
+def automagic_install(
+    ip_settings=ip_conf_default_d,
+    ap_settings=hostapd_conf_default_d,
+    dns_settings=dnsmasq_conf_default_d):
+    
+    # Create an instance of Init and Install classes
+    init = BerryInit()
+    config = BerryConfig()
+
+    # Install dependencies
+    init.dep_install()
+
+    # Modify dhcpcd.conf to ignore selected interface
+    init.mod_dhcpcd(ap_settings['interface'])
+
+    # Configure static IP settings @ /etc/network/interfaces
+    config.ipconf(ip_settings)
+
+    # Restart dhcpcd and reload config for interface
+    init.service_reload()
+
+    # Configure hostapd @ /etc/hostapd/hostapd.conf
+    config.hostapd_conf(ap_settings)
+
+    # Configure dnsmasq @ /etc/dnsmasq.conf
+    config.dnsmasq_conf(dns_settings)
+
+    # Enable IPv4 forwarding and configure NAT
+    init.ipv4_forward()
+    init.net_conf()
+    
 
 
 def wizard_install():
